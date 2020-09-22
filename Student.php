5e3594f04250ddf9fdbb2208fca147ab85ce1e83
LiuManH <2639089532@qq.com>
2020-09-22 15:40:48 +0800

    Make a new PHP class,called Student

--- Student.php
+++ Student.php
@@ -13,7 +13,7 @@
  */
 class Student {
 
-    function __construct() {s
+    function __construct() {
         $this->surname = 'Liu';
         $this->first_name = 'ManHua';
         $this->emails = array('2639089532@qq.com');
